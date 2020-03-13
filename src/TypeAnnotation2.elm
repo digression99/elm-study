fd6c1486b6b68b1ad1f478d24ca9590e012a3056
Pseudocoder Kim <digression99@gmail.com>
2020-03-13 10:30:16 +0900

    Add type exercises

--- src/TypeAnnotation2.elm
+++ src/TypeAnnotation2.elm
@@ -1,6 +1,13 @@
 module TypeAnnotation2 exposing (..)
 
+import Html
 
+
+type alias Item =
+    { name : String, price : Float, qty : Int, discounted : Bool }
+
+
+cart : List Item
 cart =
     [ { name = "Lemon"
       , price = 0.5
@@ -18,3 +25,43 @@ cart =
       , discounted = False
       }
     ]
+
+
+discount : Int -> Float -> Item -> Item
+discount minQty discPct item =
+    if not item.discounted && item.qty >= minQty then
+        { item
+            | price = item.price * (1.0 - discPct)
+            , discounted = True
+        }
+
+    else
+        item
+
+
+newCart : List Item
+newCart =
+    List.map (discount 10 0.3 >> discount 5 0.2) cart
+
+
+fromBool : Bool -> String
+fromBool b =
+    case b of
+        True ->
+            "True"
+
+        False ->
+            "False"
+
+
+parseCartItemToString : Item -> String
+parseCartItemToString item =
+    String.join " " [ item.name, String.fromFloat item.price, String.fromInt item.qty, fromBool item.discounted ]
+
+
+main : Html.Html msg
+main =
+    newCart
+        |> List.map parseCartItemToString
+        |> String.join ", "
+        |> Html.text
